930c6cb01abd82ca653ba4087a98c84cf2631062
halfdecay <duklv123@gmail.com>
2020-08-21 19:32:37 +0300

    check

--- readFileNplotRNZ_ARSA.m
+++ readFileNplotRNZ_ARSA.m
@@ -1,5 +1,5 @@
 function readFileNplotRNZ_ARSA()
-   filename = "C1yauza_elcetron_osc00003.txt";
+   filename = "C1yauza_elcetron_osc00010.txt";
    data = dlmread(filename, ',');
   [last_shift, column_size] = size(data);
   first_shift = 6;  
@@ -10,8 +10,42 @@ function readFileNplotRNZ_ARSA()
   sgf = sgolayfilt(volt,order,order+2);
   sgf1 = filtfilt(ones(1,10)/10,1,sgf);
   hold on
-  plot(time,sgf1,'b')
+  time_new = time(10:size(time)-10);
+  volt_new = sgf1(10:size(sgf1)-10);
+  wave=1310;
+  
+##  ------------------------------------
+  if (wave == 1550) 
+        power= volt_new *0.148315667734024+0.001240916854482;
+  endif
+  if (wave == 1310)
+      power = volt_new *0.277207551672137+0.002315948133696;
+  endif
+  if (wave == 850)
+      power = volt_new *0.987266707402605+0.01008908904627;
+  endif
+  if (wave == 1300)
+      power = volt_new *0.277207551672137+0.002315948133696;
+  endif
+  [i, iw]=min(power);
+  iter=0;
+  power_sum=0;
+  for k=1:iw
+    power_sum=power_sum+power(k);    
+    iter=iter+1;
+  endfor
+ 
+  ave_offset= power_sum/iter;
+
+  RNZ = -1*(10*log10(power)-10*log10( ave_offset));
+
+##  ------------------------------------  
+  third_ax=zeros(1,size(RNZ));
+  
+##  third_ax+1
+  plot(time_new,log(RNZ),'b')
   grid minor on
-  set(gca,"linewidth", 1,  "fontsize", 16)
-  axis([-0.0004, 0.001]);
+  
+  set(gca,"linewidth", 1,  "fontsize", 16) 
+##  axis([-0.0004, 0.001]);
 endfunction
