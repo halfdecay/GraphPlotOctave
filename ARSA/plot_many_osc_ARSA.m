08776041fd87c3030109ab8ac5f93a97fe29cfe0
halfdecay <duklv123@gmail.com>
2020-09-03 09:48:31 +0300

    weibull

--- ARSA/plot_many_osc_ARSA.m
+++ ARSA/plot_many_osc_ARSA.m
@@ -1,30 +1,31 @@
 function plot_many_osc_ARSA()
 
   f = "Arsa_data.txt";
-  [a,b,c]= importdata (f);
-  [z,k]=size(a.data);
-  Z=a.data(1:z,2);
-  T=a.data(1:z,3);
-  Umon=a.data(1:z,4);
-  wave=a.data(1:z,5);
-  oscnum=a.data(1:z,6);
-  for(i=1:1:z)
-  Dose(i)=Umon(i)*39769/(Z(i)+5.2).^2.086*2*10^8;
-  endfor
-
-
-  OSCnum = 3
-  filename1 = returnFileName(OSCnum)
   
-   filename = "C1yauza_elcetron_osc00003.txt";
-   str1= strsplit(filename,{"osc",".t"})
-  oscilNumber=str2double(str1(1,2))
+  
+  [z,k,Z,T,Umon,wave,oscnum,Dose]=getdataArsa(f);
+
+  oscnum;
+
+  
+  
+
+  OSCnum =5
+  
+  plotName=strcat("T",int2str( T(OSCnum)),"_wave",int2str( wave(OSCnum)),"_dose" ,int2str( Dose(OSCnum)/10^(8)),"e+8_osc",int2str( oscnum(OSCnum)),".png")         ;
+  Umon(OSCnum);
+  T(OSCnum);
+  wave(OSCnum);
+  oscnum(OSCnum);
+  filename = returnFileName(oscnum(OSCnum));
+  str1= strsplit(filename,{"osc",".t"});
+  oscilNumber=str2double(str1(1,2));
   dataNum= oscilNumber+1;
 
   data = dlmread(filename, ',');
   [last_shift, column_size] = size(data)
   first_shift = 6;  
-  step_read=last_shift/90000;
+  step_read=round( last_shift/90000);
   time = data(first_shift:step_read: last_shift, 1);
   volt = data(first_shift:step_read: last_shift, 2);
   order = 13;
@@ -33,11 +34,11 @@ function plot_many_osc_ARSA()
   hold on
   time_new = time(10:size(time)-10);
   volt_new = sgf1(10:size(sgf1)-10);
-  wave=1310;
+
   
 
-##  ------------------------------------
-  power = p(volt_new,wave);
+
+  power = p(volt_new,wave(OSCnum));
   
   
   [i, iw]=min(power); 
@@ -50,66 +51,56 @@ function plot_many_osc_ARSA()
 
 ##  t=time_new(iter-10:size(time_new));
   ave_offset= power_sum/iter;
-  RNZ =-10*(log10(power/ave_offset));
-
-    
-  [a,b]=size(time_new)
+  RNZ =-10*(log10(power/ave_offset));    
+  [a,b]=size(time_new);
   [c,d]= max(RNZ);
-  
-##  k_new(d),time_k_new(d),k_new(a/2),time_k_new(a/2),k_new(a),time_new(a)
-
-
   k=1;
   l=1;
-##    x0= [k,l,RNZ(round(d*0.01)),time_new(round(d*0.01)),RNZ(round(a/2)),time_new(round(a/2)),RNZ(a),time_new(a),RNZ(round(d*0.3)),time_new(round(d*0.3))]
-  x0= [k,l,RNZ(round(iter*1.1)),time_new(round(iter*1.1)),RNZ(round(a/2)),time_new(round(a/2)),RNZ(round(a*0.9)),time_new(round(a*0.9)),RNZ(round(d*0.3)),time_new(round(d*0.3))];
-##x0= [k,l,RNZ(d),time_new(d),RNZ(a/2),time_new(a/2),RNZ(a),time_new(a)]
-##gvar
-##gvar;
-[x, fval, info] = fsolve (@f,x0);
-k=x(1);
-l=x(2);
+  amp = 50
+  x0= [k,l,(RNZ(round(iter*1.05)+amp)+ RNZ(round(iter*1.05))+RNZ(round(iter*1.05)-amp))/3,time_new(round(iter*1.05)),(RNZ(round(a/2))+RNZ(round(a/2)-amp)+RNZ(round(a/2)+amp))/3,time_new(round(a/2)),(RNZ(round(a*0.9)+amp)+RNZ(round(a*0.9))+RNZ(round(a*0.9)-amp))/3,time_new(round(a*0.9)),RNZ(round(d*0.3)),time_new(round(d*0.3))]
+  [x, fval, info] = fsolve (@f,x0);
+  k=x(1);
+  l=x(2);
 
-for(i=1:1:a)
- 
- t_WB(i)= time_new(i);
- if(t_WB(i)<=0)
-  y(i) = 0;
-else
-  y(i) = k/l*(t_WB(i)/l).^(k-1).*exp(-(t_WB(i)./l).^k);
- endif
- 
- 
-endfor
-
-##y = k/l*(t_WB/l).^(k-1).*exp(-(t_WB./l).^k);
+  for(i=1:1:a)
+    t_WB(i)= time_new(i);
+    if(t_WB(i)<=0)
+    y(i) = 0;
+    else
+    y(i) = k/l*(t_WB(i)/l).^(k-1).*exp(-(t_WB(i)./l).^k);
+    endif
+  endfor
   
-  plot(time_new,RNZ,'b')
-  plot(time_new,y,'r')
+  sgf3 = filtfilt(ones(1,10)/10,1,y);
+  hf = figure ();
+  plot(time_new,RNZ,'b',time_new,sgf3,'r');
   grid minor on
-##   axis([-0.00004, 0.002,-0.05,0.7]);
+  ylabel ("RNZ, dB");
+  xlabel ( "T, s");  
+  mainPath=pwd
+  cd ("Graphs")
+  
+  print (hf, plotName,"-solid");
+  cd (mainPath)
+  
+
+  
 endfunction
 
 
 
 
 function  y = f (x)
-
   y = zeros (3, 1);
-persistent R1= x(3);
-persistent R2= x(5);
-persistent R3= x(5);
-##persistent R4= x(7);
-persistent T1= x(4);
-persistent T2= x(6);
-persistent T3= x(8);
-##persistent T4= x(10)
-y(1) = x(1)/x(2)*(T1/x(2))^(x(1)-1)*exp(-(T1/x(2))^x(1))-R1;
-y(2) = x(1)/x(2)*(T2/x(2))^(x(1)-1)*exp(-(T2/x(2))^x(1))-R2;
-y(3) = x(1)/x(2)*(T3/x(2))^(x(1)-1)*exp(-(T3/x(2))^x(1))-R3;
-##y(4) = x(1)/x(2)*(T4/x(2))^(x(1)-1)*exp(-(T4/x(2))^x(1))-R4;
-
-
+  persistent R1= x(3);
+  persistent R2= x(5);
+  persistent R3= x(5);
+  persistent T1= x(4);
+  persistent T2= x(6);
+  persistent T3= x(8);
+  y(1) = x(1)/x(2)*(T1/x(2))^(x(1)-1)*exp(-(T1/x(2))^x(1))-R1;
+  y(2) = x(1)/x(2)*(T2/x(2))^(x(1)-1)*exp(-(T2/x(2))^x(1))-R2;
+  y(3) = x(1)/x(2)*(T3/x(2))^(x(1)-1)*exp(-(T3/x(2))^x(1))-R3;
 endfunction
 
 
@@ -131,36 +122,32 @@ endfunction
 
 
 function    filename = returnFileName(OSCnum)
-  
-   filename= "test";
-   [s1,s2]=size(ls);
-   listOfOSc= dir();
+  [s1,s2]=size(ls);
+  listOfOSc= dir();
+  for(i=3:1:s1)
+  numOflist(i)= str2double(strsplit(listOfOSc(i).name,{"osc",".t"})(1,2));
+  endfor
+  [s3,s4] = size(numOflist);
 
-   for(i=3:1:s1)
-   numOflist(i)= str2double(strsplit(listOfOSc(i).name,{"osc",".t"})(1,2))     ;
-   endfor
-   
-   [s3,s4] = size(numOflist);
+  for(k=3:1:s4)
+  if(OSCnum- numOflist(k)==0)
+  filename= listOfOSc(k).name  
+  endif
+endfor
 
-    for(k=3:1:s4)
-    k
-    OSCnum
-    numOflist(k)
-      if(OSCnum- numOflist(k)==0)
-      numOflist(k)
-      filename= listOfOSc(k).name  
-      endif
-##
-    endfor
-filename   
-   
-   
 
- 
-   
-##   strsplit(listOfOSc,"\n")
-##  str1= strsplit(filename,{"osc",".t"})
-##  oscilNumber=str2double(str1(1,2))
-##  dataNum= oscilNumber+1;
-##  
+endfunction
+
+
+function    [z,k,Z,T,Umon,wave,oscnum,Dose]=getdataArsa(f)
+  [a,b,c]= importdata (f);
+  [z,k]=size(a.data);
+  Z=a.data(1:z,2);
+  T=a.data(1:z,3);
+  Umon=a.data(1:z,4);
+  wave=a.data(1:z,5);
+  oscnum=a.data(1:z,6);
+  for(i=1:1:z)
+  Dose(i)=Umon(i)*39769/(Z(i)+5.2).^2.086*2*10^8;
+  endfor
 endfunction
